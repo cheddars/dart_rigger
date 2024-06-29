b9c31dd898c132697e48cb9f22c23f57daabcadc
cheddars <nezahrish@gmail.com>
2024-06-29 22:00:09 +0900

    api key from config

--- test/dartapi_test.py
+++ test/dartapi_test.py
@@ -1,19 +1,31 @@
+import configparser
+import unittest
+
 import logging
 from cache import RedisCache, MemoryCache
 from dartrig import DartAPI
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(name)-10s [%(levelname)s] %(message)s")
 
-def test_list():
-    #cache = RedisCache(host="localhost", port=6380, db=0, expire_mins=60*24, clear_cache=True)
-    cache = MemoryCache()
-    dartapi = DartAPI(keys=["7bfdb16b437110bfa8ded07b9ca22d173944c0c3"], cache=cache)
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-    r = dartapi.get_disclosure_list(end_de="20200102", max_page=10, use_cache=True)
-    logging.debug(f"result A : {r}")
+key = config["DART"]["API_KEY"]
+class DartAPITest(unittest.TestCase):
 
-    r = dartapi.get_disclosure_list(end_de="20200102", max_page=10, use_cache=True)
-    logging.debug(f"result B : {r}")
+    def test_list(self):
+        #cache = RedisCache(host="localhost", port=6380, db=0, expire_mins=60*24, clear_cache=True)
+        cache = MemoryCache()
+        dartapi = DartAPI(keys=[key], cache=cache)
 
-if __name__ == "__main__":
-    test_list()
+        r = dartapi.get_disclosure_list(end_de="20200102", max_page=10, use_cache=True)
+        logging.debug(f"result A : {r}")
+
+        r = dartapi.get_disclosure_list(end_de="20200102", max_page=10, use_cache=True)
+        logging.debug(f"result B : {r}")
+
+    def test_detail(self):
+        #cache = RedisCache(host="localhost", port=6380, db=0, expire_mins=60*24, clear_cache=True)
+        cache = MemoryCache()
+        dartapi = DartAPI(keys=[key], cache=cache)
+
